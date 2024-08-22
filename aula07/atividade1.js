1dcb06921cfd930c6dee5ac12fd4d1912d6ead00
samuel <samuel.l.silva9@aluno.senai.br>
2024-08-22 12:18:46 -0300

    Atualizando o repositório

---
+++ aula07/atividade1.js
@@ -0,0 +1,9 @@
+let dolar, real, conversao
+
+dolar = parseFloat (prompt("digite a sua quantia em dolares: "))
+
+real = parseFloat (prompt("digite a sua quantia em reais: "))
+
+conversao = dolar*real
+
+document.write(`${conversao} dolares`)
